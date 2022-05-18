3415af7c204bfcd7a6d041ffb39e25602c127b29
Jacob Caggese <jcaggese@gmail.com>
2022-05-18 16:06:56 -0400

    GUI Error Checking
    GUI now checks for input errors

--- src/main/java/pkg/boardingpass/FormController.java
+++ src/main/java/pkg/boardingpass/FormController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FormController {
     @FXML
@@ -23,18 +26,42 @@ public class FormController {
     SplitMenuButton destination;
     @FXML
     DatePicker departure;
+    @FXML
+    TextField departTime;
+    @FXML
+    Label error;
 
     @FXML
     protected void submit() throws IOException {
-        //run BoardingPass functions from here
-        System.out.println(name.getText());
-        System.out.println(email.getText());
-        System.out.println(phone.getText());
-        System.out.println(gender.getText());
-        System.out.println(age.getText());
-        System.out.println(date.getValue());
-        System.out.println(destination.getText());
-        System.out.println(departure.getValue());
+        //check if all fields filled out properly
+        if(name.getText().isBlank())
+            error.setText("Please input your name");
+        else if(!checkEmail(email.getText()))
+            error.setText("Incorrect email format. Should be xxx@yyy.com.");
+        else if(!checkPhone(phone.getText()))
+            error.setText("Incorrect Phone Number. Format should be ten numbers.");
+        else if(gender.getText().equals("Select"))
+            error.setText("Please select a gender.");
+        else if(!checkAge(age.getText()))
+            error.setText("Please input a number for age.");
+        else if (date.getValue() == null)
+            error.setText("Please select the current date.");
+        else if (destination.getText().equals("Select"))
+            error.setText("Please select a destination.");
+        else if (!checkDeparture(departure.getValue(), departTime.getText()))
+            error.setText("Please select a date of departure after the current date with time in format: (XX:XX).");
+        else {
+            error.setText("");
+            //run BoardingPass functions from here
+            System.out.println(name.getText());
+            System.out.println(email.getText());
+            System.out.println(phone.getText());
+            System.out.println(gender.getText());
+            System.out.println(age.getText());
+            System.out.println(date.getValue());
+            System.out.println(destination.getText());
+            System.out.println(departure.getValue());
+        }
     }
 
     @FXML
@@ -47,5 +74,44 @@ public class FormController {
     protected void changeValDest(ActionEvent e) throws IOException {
         MenuItem choice = (MenuItem) e.getSource();
         destination.setText(choice.getText());
+    }
+
+    private boolean checkEmail(String email) {
+        if (email.isBlank())
+            return false;
+        Matcher match = Pattern.compile("\\w+@\\w+.com").matcher(email);
+        if (match.matches())
+            return true;
+        return false;
+    }
+
+    private boolean checkPhone(String phone) {
+        Matcher match = Pattern.compile("\\d{10}").matcher(phone);
+        if (match.find())
+            return true;
+        return false;
+    }
+
+    private boolean checkAge(String age) {
+        if (age.isBlank())
+            return false;
+        Matcher match = Pattern.compile("\\d+").matcher(age);
+        if (match.matches())
+            return true;
+        return false;
+    }
+
+    private boolean checkDeparture(LocalDate departure, String departTime) {
+        if (departure == null)
+            return false;
+        Matcher match = Pattern.compile("\\d\\d:\\d\\d").matcher(departTime);
+        if (!match.matches())
+            return false;
+        String[] time = departTime.split(":");
+        if (Integer.parseInt(time[0]) > 23 || Integer.parseInt(time[1]) > 59)
+            return false;
+        if (departure.compareTo(date.getValue()) >= 0) //depart after current date
+            return true;
+        return false;
     }
 }
