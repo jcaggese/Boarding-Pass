c8cc8f05cdcce4c38da8efccf8657ea0725aa8a8
Jacob Caggese <jcaggese@gmail.com>
2022-05-19 15:20:43 -0400

    Added Unique Pass Number Generation
    Also created Boarding Pass Constructor and Boarding pass JUnit file.

---
+++ src/main/Tests/pkg/boardingpass/BoardingPassTest.java
@@ -0,0 +1,19 @@
+package pkg.boardingpass;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardingPassTest {
+    BoardingPass pass;
+
+    @BeforeEach
+    void setUp() {
+        BoardingPass pass = new BoardingPass("Tester", "Tester@test.com", "0987654321",
+                "Test", "85", LocalDate.of(2012, 12, 11), "LAX",
+                LocalDateTime.of(2013, 4, 10, 8, 30));
+    }
+}
--- src/main/java/pkg/boardingpass/BoardingPass.java
+++ src/main/java/pkg/boardingpass/BoardingPass.java
@@ -1,11 +1,50 @@
 package pkg.boardingpass;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 /**
  * Creates boarding pass from input data
  * Stores all information in a file
  */
 public class BoardingPass {
-    public BoardingPass() {
+    private String origin;
+    private String name;
+    private String email;
+    private String phoneNum;
+    private String gender;
+    private String age;
+    private LocalDate date;
+    private String destination;
+    private LocalDateTime departure;
+    private String passID;
+    private LocalDateTime ETA;
 
+    public BoardingPass(String name, String email, String phoneNum, String gender, String age, LocalDate date,
+                        String destination, LocalDateTime departure) {
+        origin = "JFK";
+        this.name = name;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.gender = gender;
+        this.age = age;
+        this.date = date;
+        this.destination = destination;
+        this.departure = departure;
+        passID = generateID();
     }
+
+    /**
+     * Uses UUID to generate unique ID for boarding pass
+     * Uses hashcode to turn that UUID into a number
+     * Could use BigInteger, if hashing collisions are a problem
+     * but the smaller size of an int is preferred for readability
+     * @return
+     */
+    private String generateID() {
+        return UUID.randomUUID().hashCode() + "";
+    }
+
+
 }
