34ce391c0e6f6463cee73061002b522196f77ccd
DeRose Brutus <derose_brutus@hotmail.com>
2022-05-22 17:52:09 -0500

    Add tests to check prices

--- src/main/Tests/pkg/boardingpass/ETATest.java
+++ src/main/Tests/pkg/boardingpass/ETATest.java
@@ -9,11 +9,11 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ETATest {
-    ETA time;
+    ETA eta;
 
     @BeforeEach
     void setUp() {
-        time = new ETA();
+        eta = new ETA();
     }
 
 
@@ -21,8 +21,27 @@ class ETATest {
     @Test
     void calculate()
     {
+        System.out.println("ETA calculation test! \n");
         LocalDateTime test = LocalDateTime.now();
-        System.out.println(time.Calculate(test, "LAX"));
+        System.out.println(eta.CalculateETA(test, "LAX"));
+        System.out.println("Flight duration is: " + eta.GetDuration());
+    }
+
+    @Test
+    void calculatePrice()
+    {
+        System.out.println("Calculate price test! \n");
+        String nonexistent = "0";
+        String young = "12";
+        String adult = "18";
+        String old = "60";
+        String time = "370";
+        String boy = "Male";
+        String girl = "Female";
+        System.out.println(eta.CalculatePrice(nonexistent, time, boy));
+        System.out.println(eta.CalculatePrice(young, time, girl));
+        System.out.println(eta.CalculatePrice(adult, time, girl));
+        System.out.println(eta.CalculatePrice(old, time, girl));
     }
 
     @AfterEach
